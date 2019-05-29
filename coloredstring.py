77baa8893eb937c33a3ef7d921f8178d9c086365
Maktm <michaelakiros@gmail.com>
2019-05-29 02:23:31 -0500

    Rewrites half of the code

---
+++ coloredstring.py
@@ -0,0 +1,128 @@
+"""The ColoredString class (provided as `cs` for ease-of-use) is
+used to generate strings that will be printed with colors in a
+terminal that supports ANSI color codes. The color highlighting
+syntax is as follows:
+
+Using a marker, you can specify where you want to start coloring
+in the text. A marker is denoted by a caret symbol (^) followed
+by a color ID. Optionally, you can append a repeater, in the
+format of '*[1, 2, 3, ...]' to specify how many characters should
+be highlighted by the marker that precedes it.
+
+                Color IDs
+        --------------------------
+Color   Foreground      Background
+-----   ----------      ----------
+white       0               a
+green       1               b
+red         2               c
+yellow      3               d
+blue        4               e
+cyan        5               f
+white       6               g
+magenta     7               h
+black       8               i
+
+Other color IDs include:
+
+Color ID    Use
+--------    ---
+.           Set to default foreground
+,           Set to default background
+:           Set to default foreground & background
+~           Set to complement of foreground & background
+
+Examples:
+
+    ^1Hello             prints "Hello" in green
+    ^1H^aello           prints "H" in green and "ello" in the default color with a white background
+    ^5*3Hello           prints "Hel" in cyan and the rest with the default color
+    ^1Hello ^:world     prints "Hello " in green and resets it so that "world" prints with the default foreground
+"""
+
+import re
+
+from typing import Tuple, Match
+
+class UnsafeRepeaterException(Exception):
+    """Used to let the user know that a repeater with a bad
+    count value was specified. Running that repeater against
+    the ColoredString class would likely cause it to crash.
+    """
+    def __init__(self, count) -> None:
+        """Initializes an UnsafeRepeaterException by storing the
+        count value that caused a red flag."""
+        super().__init__("Unsafe repeater was specified (out-of-bounds)")
+
+        self.count = count
+
+class ColoredString:
+    """Specify a string using the syntax stated above and this
+    class will be able to convert the string into an ANSI color
+    coded string with the use of the __str__ method.
+    """
+    # Regular expressions / patterns
+    _color_pattern = r'\^[a-i0-8]'
+    _repeater_pattern = r'[*]+[1-9]+'
+    _color_and_repeater_pattern = ''.join([_color_pattern, _repeater_pattern])
+
+    # Compiled regular expressions
+    _color_regexp = re.compile(_color_pattern)
+    _repeater_regexp = re.compile(_repeater_pattern)
+    _color_and_repeater_regexp = re.compile(_color_and_repeater_pattern)
+
+    def __init__(self, string: str) -> None:
+        """Initialize an instance of ColoredString."""
+        self.string = string
+
+    def __str__(self) -> str:
+        """Converts self.string from our syntax into an ANSI
+        colored coded string ready for printing to the terminal.
+        """
+        return self.process(self.string)
+
+    def process(self, string: str) -> str:
+        """Process self.string by applying the following rules
+        and returns an ANSI color code compliant string:
+
+            1. Check all instances of repeaters for safety (not out-of-bounds)
+            2. Replace all instances of repeaters with the ANSI color reset code
+            3. Replace all instances of color specifiers with their ANSI color codes
+        """
+        self._check_repeater_safety(string)
+        no_repeaters_str = self._process_repeaters(string)
+        ansi_compliant_str = self._process_colors(no_repeaters_str)
+
+        return ansi_compliant_str
+
+    def _check_repeater_safety(self, string: str) -> None:
+        """Iterates through all the repeaters in the string of the
+        format *[count] where count = 1, 2, 3, etc. and ensures that
+        there are no count values that are out-of-bound.
+        """
+        for match in self._color_and_repeater_regexp.finditer(string):
+            unsafe_check_info = self._is_unsafe_repeater(match)
+            if unsafe_check_info[0]:
+                raise UnsafeRepeaterException(unsafe_check_info[1])
+
+    def _is_unsafe_repeater(self, match: Match) -> Tuple[bool, int]:
+        """Determine whether the matched repeater is a safe repeater
+        by checking whether the count index is out-of-bounds.
+        """
+        return False, -1 # TODO: Placeholder, change!
+
+    def _process_repeaters(self, string: str) -> str:
+        """Replaces all instances of repeaters with an ANSI color
+        reset code at the 'count' position specified by the repeater.
+        """
+        return string # TODO: Placeholder, change!
+
+    def _process_colors(self, string: str) -> str:
+        """Replaces all instances of color specifiers in the format
+        of ^[color] with the ANSI color equivalent.
+        """
+        return string # TODO: Placeholder, change!
+
+
+# Provided for ease-of-typing.
+cs = ColoredString
